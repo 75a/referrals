1703b20b08b0e51e7d73207afc18697a2abe9649
Michal Brzozowski <contact@michalbrzozowski.com>
2020-10-27 17:55:20 +0100

    Add the first part of the reflink URL to the Config

--- Config.php
+++ Config.php
@@ -5,6 +5,7 @@ class Config {
     public static function getConfig(){
         return [
             'userClass' => \app\models\User::class,
+            'reflinkURL' => "http://localhost:8080/r?code=",
             'db' => [
                 'dsn' => $_ENV['DB_DSN'],
                 'user' => $_ENV['DB_USER'],
