06ba7550adf5b0d45d3c9e8caea5c17078a29a70
Michal Brzozowski <contact@michalbrzozowski.com>
2021-02-24 08:25:51 +0100

    Remove first name and last name from the register form view

--- views/register.php
+++ views/register.php
@@ -10,8 +10,6 @@ use app\core\CSRFProtector;
 
 <?php $form = \app\core\form\Form::begin('',"post") ?>
     <?php echo $form->getCSRFField(CSRFProtector::getToken()) ?>
-    <?php echo $form->field($model, 'firstname') ?>
-    <?php echo $form->field($model, 'lastname') ?>
     <?php echo $form->field($model, 'email') ?>
     <?php echo $form->field($model, 'password')->passwordField() ?>
     <?php echo $form->field($model, 'confirmPassword')->passwordField() ?>
