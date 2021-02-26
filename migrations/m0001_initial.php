28bd4223b20bb86d69e190ad68afcb5d5a6fa75d
Michal Brzozowski <contact@michalbrzozowski.com>
2021-02-26 02:29:43 +0100

    Rebuild the database and merge migrations

--- migrations/m0001_initial.php
+++ migrations/m0001_initial.php
@@ -4,21 +4,30 @@ class m0001_initial{
     public function up(): void
     {
         $db = \app\core\Application::$app->db;
-        $SQL = "CREATE TABLE users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            email VARCHAR(255) NOT NULL,
-            firstname VARCHAR(255) NOT NULL,
-            lastname VARCHAR(255) NOT NULL,
-            status TINYINT NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        ) ENGINE=INNODB;";
+        $SQL = "
+            CREATE TABLE users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                email VARCHAR(255) NOT NULL,
+                status TINYINT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                password VARCHAR(255) NOT NULL,
+                referral_code VARCHAR(8) NOT NULL,
+                points INT NOT NULL DEFAULT 0
+            ) ENGINE=INNODB;
+            CREATE TABLE refclick (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                user_id INT NOT NULL,
+                clicker_ip VARCHAR(255) NOT NULL,
+                FOREIGN KEY (user_id) REFERENCES users(id)
+            ) ENGINE=INNODB;
+        ";
         $db->pdo->exec($SQL);
     }
 
     public function down(): void
     {
         $db = \app\core\Application::$app->db;
-        $SQL = "DROP TABLE users;";
+        $SQL = "DROP TABLE refclick;DROP TABLE users;";
         $db->pdo->exec($SQL);
     }
 }
