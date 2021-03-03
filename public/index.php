117e0696449375330f891cccc9e9b3c29c752a06
Michal Brzozowski <contact@michalbrzozowski.com>
2021-03-03 01:29:50 +0100

    Add errors rounting

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use app\controllers\ContactController;
+use app\controllers\ErrorController;
 use app\controllers\ProfileController;
 use app\controllers\ReferralController;
 use app\controllers\SiteController;
@@ -10,28 +10,29 @@ use app\Config;
 use app\locales\English;
 use app\locales\Polish;
 
-
 require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
-
 $config = Config::getConfig();
 
 $app = new Application(dirname(__DIR__), $config);
-
-$app->language = new Polish();
+$app->language = English::language();
 
 $app->router->get('/', [SiteController::class, 'home']);
 $app->router->get('/r', [ReferralController::class, 'refclick']);
 
-$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->get('/login', [AuthController::class, 'showLogin']);
 $app->router->post('/login', [AuthController::class, 'login']);
+
+$app->router->get('/register', [AuthController::class, 'showRegister']);
+$app->router->post('/register', [AuthController::class, 'register']);
 
 $app->router->get('/logout', [AuthController::class, 'logout']);
 
-$app->router->get('/register', [AuthController::class, 'register']);
-$app->router->post('/register', [AuthController::class, 'register']);
-
 $app->router->get('/profile', [ProfileController::class, 'profile']);
+
+
+$app->router->onErrors(["400", "401", "402", "403", "404", "405"], [ErrorController::class, 'httpError']);
+$app->router->onErrorDefault([ErrorController::class, 'error']);
 
 $app->run();
