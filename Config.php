df3e53de60e4bde1fa7e7f942fd0b3da4465859a
Michal Brzozowski <contact@michalbrzozowski.com>
2021-03-03 01:18:05 +0100

    Remove unnecessary data from config class

--- Config.php
+++ Config.php
@@ -4,8 +4,6 @@ namespace app;
 class Config {
     public static function getConfig(){
         return [
-            'userClass' => \app\models\User::class,
-            'reflinkURL' => "http://localhost:8080/r?code=",
             'db' => [
                 'dsn' => $_ENV['DB_DSN'],
                 'user' => $_ENV['DB_USER'],
