b91f109c7dba05f60468ca03864d8c01df29d4b4
Michal Brzozowski <contact@michalbrzozowski.com>
2021-03-03 01:24:50 +0100

    Clean up existing models and remove the old structure leftovers

--- models/RefClick.php
+++ models/RefClick.php
@@ -3,18 +3,26 @@
 namespace app\models;
 
 use app\core\db\DbModel;
+use app\core\Model;
 use app\core\Utils;
 
-class RefClick extends DbModel
+class RefClick extends Model
 {
-    public const REF_LENGTH = 8;
+    public const TABLE_NAME = "refclick";
+    public const PRIMARY_KEY = "id";
+    public const ATTRIBUTES = ['user_id','clicker_ip'];
+    public const RULES = [
+        'user_id' => [self::RULE_REQUIRED],
+        'clicker_ip' => [self::RULE_REQUIRED]
+    ];
 
-    public DbModel $refUser;
+    const REF_LENGTH = 8;
 
+    public User $refUser;
+
+    public int $id;
     public int $user_id;
     public string $clicker_ip;
-
-    public bool $isSaved = false;
 
     public static function generateNewReferralCode(): string
     {
@@ -24,67 +32,5 @@ class RefClick extends DbModel
     public static function isValidReferralCode(string $potentialReferralCode): bool
     {
         return (ctype_alnum($potentialReferralCode) && (strlen($potentialReferralCode) == self::REF_LENGTH));
-    }
-
-    public function save(): bool
-    {
-        if ($this->user_id && $this->clicker_ip) {
-
-            $existingClick = (new RefClick)->findOne([
-                'user_id' => $this->user_id,
-                'clicker_ip' => $this->clicker_ip
-            ]);
-            if (!$existingClick){
-                $this->isSaved = true;
-                $this->refUser->addPoints(1);
-
-                return parent::save();
-            }
-        }
-        return false;
-    }
-
-    public function isSaved(): bool
-    {
-        return $this->isSaved;
-    }
-
-    public function rules(): array
-    {
-        return [
-            'user_id' => [self::RULE_REQUIRED],
-            'clicker_ip' => [self::RULE_REQUIRED]
-        ];
-    }
-
-    public function tableName(): string
-    {
-        return "refclick";
-    }
-
-    public function attributes(): array
-    {
-        return ['user_id','clicker_ip'];
-    }
-
-    public function primaryKey(): string
-    {
-        return 'id';
-    }
-
-    public function setReferralCode(String $referralCode): bool
-    {
-        $user = (new User)->findOne(['referral_code' => $referralCode]);
-        if ($user){
-            $this->refUser = $user;
-            $this->user_id = $user->id;
-            return true;
-        }
-        return false;
-    }
-
-    public function setIP(String $ip): void
-    {
-        $this->clicker_ip = $ip;
     }
 }
--- models/User.php
+++ models/User.php
@@ -2,72 +2,58 @@
 
 namespace app\models;
 
-use app\core\Application;
 use app\core\db\DbModel;
-
 use app\core\Model;
-use app\core\UserModel;
-use app\core\Utils;
 
-class User extends UserModel
+class User extends Model
 {
+    public const TABLE_NAME = "users";
+    public const PRIMARY_KEY = "id";
+    public const ATTRIBUTES = [
+        'email',
+        'password',
+        'status',
+        'created_at',
+        'referral_code',
+        'points'
+    ];
+
+    public const RULES = [
+        'email' => [
+            self::RULE_REQUIRED,
+            self::RULE_EMAIL,
+            [self::RULE_UNIQUE, 'class' => User::class]
+        ],
+        'password' => [
+            self::RULE_REQUIRED,
+            [self::RULE_MIN, 'min' => 8],
+            [self::RULE_MAX, 'max' => 24]
+        ],
+        'confirmPassword' => [
+            self::RULE_REQUIRED,
+            [self::RULE_MATCH, 'match' => 'password']
+        ],
+    ];
+
+    public const LABELS = [
+        'email' => "E-mail",
+        'password' => "Password",
+        'confirmPassword' => "Repeat password"
+    ];
+
     const STATUS_INACTIVE = 0;
     const STATUS_ACTIVE = 1;
     const STATUS_DELETED = 2;
 
-
     public int $id;
-
     public string $email = '';
     public int $status = self::STATUS_INACTIVE;
+    public string $created_at;
     public string $password = '';
-    public string $confirmPassword = '';
     public string $referral_code = '';
     public int $points = 0;
 
-    public function tableName(): string
-    {
-        return 'users';
-    }
-
-    public function primaryKey(): string
-    {
-        return 'id';
-    }
-
-    public function save(): bool
-    {
-        $this->status = self::STATUS_INACTIVE;
-        $this->password = strval(password_hash($this->password, PASSWORD_DEFAULT));
-        $this->setReferralCode();
-        return parent::save();
-    }
-
-
-    public function rules(): array
-    {
-        return [
-            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL, [
-                self::RULE_UNIQUE, 'class' => self::class
-            ]],
-            'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8], [self::RULE_MAX, 'max' => 24]],
-            'confirmPassword' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']],
-        ];
-    }
-
-    public function attributes(): array
-    {
-        return ['email', 'password', 'status', 'referral_code', 'points'];
-    }
-
-    public function labels(): array
-    {
-        return [
-            'email' => Application::$app->getText("E-mail"),
-            'password' => Application::$app->getText("Password"),
-            'confirmPassword' => Application::$app->getText("Repeat password")
-        ];
-    }
+    public string $confirmPassword = '';
 
     public function getDisplayName(): string
     {
@@ -89,6 +75,5 @@ class User extends UserModel
     public function addPoints(int $points)
     {
         $this->points += $points;
-        parent::updateColumn('points');
     }
 }
