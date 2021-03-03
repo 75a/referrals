7f83784f002b68cf6c809f1f56e292518b26021f
Michal Brzozowski <contact@michalbrzozowski.com>
2021-03-03 01:27:36 +0100

    Create status code getter method

--- core/Response.php
+++ core/Response.php
@@ -4,9 +4,16 @@ namespace app\core;
 
 class Response
 {
+    public string $message = "";
+
     public function setStatusCode(int $code): void
     {
         http_response_code($code);
+    }
+
+    public function getStatusCode(): int
+    {
+        return http_response_code();
     }
 
     public function redirect(string $url): void
