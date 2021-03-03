0706a7c80d7d7c90fb4b0e6de166afd91e0b216d
Michal Brzozowski <contact@michalbrzozowski.com>
2021-03-03 01:23:59 +0100

    Make the site controller use the view builder

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,41 +3,33 @@
 namespace app\controllers;
 use app\core\Application;
 use app\core\Controller;
-use app\core\CSRFProtector;
-use app\core\middlewares\LoggedInMiddleware;
-use app\core\Request;
-use app\core\Response;
-use app\locales\English;
-use app\locales\Polish;
-use app\models\ContactForm;
-use app\models\RefClick;
-use app\models\User;
+use app\core\views\builder\SingleLayoutYieldableViewBuilder;
 
 class SiteController extends Controller
 {
-
-
-    public function home()
+    public function home(): string
     {
-        if (Application::$app->user){
+        if (!Application::isGuest()){
             return $this->homeForRegistered();
         } else {
             return $this->homeForGuest();
         }
     }
 
-    private function homeForRegistered()
+    private function homeForRegistered(): string
     {
-        $app = Application::$app;
-        $params = [
+        $view = (new SingleLayoutYieldableViewBuilder())->get("userHome", [
             'userName' => Application::$app->user->getDisplayName()
-        ];
-        return $this->render('userHome', $params);
+        ]);
+        return $view->getBuffer();
     }
 
-    private function homeForGuest()
+    private function homeForGuest(): string
     {
-        return $this->render('guestHome');
+        $view = (new SingleLayoutYieldableViewBuilder())->get("guestHome", [
+            'userName' => "Guest"
+        ]);
+        return $view->getBuffer();
     }
 
 }
