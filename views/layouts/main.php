bd84fb5910ae64f2dd0d25a3170d9b84d4cf185e
Michal Brzozowski <contact@michalbrzozowski.com>
2021-03-03 01:29:29 +0100

    Make the layout template use the translating helper function

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,14 +1,13 @@
 <?php
-
-use app\core\Application;
-
-$texts = Application::$app->language->getTexts();
+    /** @noinspection PhpUndefinedVariableInspection */
+    use function app\_;
 ?>
+
 <!doctype html>
-<html lang="en">
+<html lang="<?=$htmlLang?>">
 <head>
     <meta charset="utf-8">
-    <title><?= $this->title ?></title>
+    <title><?=$metaTitle?></title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
@@ -17,30 +16,28 @@ $texts = Application::$app->language->getTexts();
             <h1>Referral Script</h1>
             <nav>
                 <ul class="top-menu mt-8 mb-8">
-                    <li class="mr-15"><a href="/" class="text-dark nav-button"><?= $texts['Home'] ?></a></li>
-                    <?php if (Application::isGuest()): ?>
-                        <li class="mr-15"><a href="/login" class="text-dark nav-button"><?= $texts['Login'] ?></a></li>
-                        <li class="mr-15"><a href="register" class="text-dark nav-button"><?= $texts['Register'] ?></a></li>
+                    <li class="mr-15"><a href="/" class="text-dark nav-button"><?= _('Home')?></a></li>
+                    <?php if ($isGuestUser): ?>
+                        <li class="mr-15"><a href="/login" class="text-dark nav-button"><?=_('Login')?></a></li>
+                        <li class="mr-15"><a href="register" class="text-dark nav-button"><?=_('Register')?></a></li>
                     <?php else: ?>
-                        <li class="mr-15"><a href="/profile" class="text-dark nav-button"><?= $texts['Profile'] ?></a></li>
-                        <li class="mr-15"><a href="/logout" class="text-dark nav-button"><?= $texts['Logout'] ?></a></li>
+                        <li class="mr-15"><a href="/profile" class="text-dark nav-button"><?=_('Profile')?></a></li>
+                        <li class="mr-15"><a href="/logout" class="text-dark nav-button"><?=_('Logout')?></a></li>
                     <?php endif; ?>
                 </ul>
                 <p class="text-small">
-                <?php if (Application::isGuest()): ?>
-                    <?= $texts['You are not logged in'] ?>
+                <?php if ($isGuestUser): ?>
+                    <?=_('You are not logged in')?>
                 <?php else: ?>
-                    <?= $texts['You are logged in as'] ?> <span class="text-bold"><?php echo Application::$app->user->getDisplayName() ?></span>
+                    <?=_('You are logged in as')?> <span class="text-bold"><?=$userDisplayName?></span>
                 <?php endif; ?>
                 </p>
             </nav>
-            <?php if (Application::$app->session->getFlash('info') !== ""): ?>
-
-                    <p class="flash">
-                        <?= $texts["Info"] ?>: <strong><?php echo Application::$app->session->getFlash('info') ?></strong>
+            <?php if ($isFlashSet): ?>
+                    <p class="flash <?=$flashStyleClass?>">
+                        <?=$flashTypeName?>: <strong><?=_($flashMessage)?></strong>
                     </p>
             <?php endif; ?>
-
         </div>
     </header>
     <main class="pt-25 pb-25">
@@ -48,12 +45,11 @@ $texts = Application::$app->language->getTexts();
             <div class="main-content">
                 {{content}}
             </div>
-
         </div>
     </main>
     <footer class="main-footer bg-dark text-bright text-bold">
         <div class="container">
-            <p><?= $texts["Michal Brzozowski 2020"] ?></p>
+            <p><?=_("Copyright &copy; 2020")?></p>
         </div>
     </footer>
 </body>
